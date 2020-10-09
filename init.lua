81504f0740a363976955d1543370623458331d33
original-Kpee <truth_to_stupid@protonmail.com>
2020-10-09 03:43:46 -0400

    Fixed addon minimap, load/unload functionality, and sharedvariables.

--- init.lua
+++ init.lua
@@ -15,13 +15,12 @@
 -- Last Updated 10.08.2020
 --  by kPee
 
-local CovenantParty_name, CovenantParty = ...
+local GlobalAddonName, CovenantParty = ...
 
 CovenantParty.version = CovenantParty.ADDON_VERSION_MAJOR .. CovenantParty.ADDON_VERSION_MINOR
 CovenantParty.passVersionCheck = false
 CovenantParty.needsAddonUpdate = false
-CovenantParty.events = {}
-CovenantParty.minimapIcon = {}
+CovenantParty.addonEnabled = true
 
 CovenantParty.messageTypes = {
     version = "__V",
@@ -31,12 +30,66 @@ CovenantParty.messageTypes = {
 
 CovenantParty.L = LibStub("AceLocale-3.0"):GetLocale("CovenantParty", true)
 
+--> Shared Variable default
+    local defaults = {
+        char = {
+            firstUse = true,
+            covenantID = nil,
+            covenantData = {
+                textureKit = nil,
+                celebrationSoundKit = nil,
+                animaChannelSelectSoundKit = nil,
+                animaChannelActiveSoundKit = nil,
+                animaGemsFullSoundKit = nil,
+                animaNewGemSoundKit = nil,
+                animaReinforceSelectSoundKit = nil,
+                name = nil,                    
+                last_betrayal = nil,
+                total_betrayals = 0,
+            }
+        },
+        profile = 
+        {
+            minimap = 
+            {
+                hide = false
+            }
+        },
+        global = 
+        {
+            region = GetLocale()
+        }
+    }
+
 -- Minimap Icon with LibDBIcon 1.0 
 --  and Library Data Broker Support with LibDataBroker 1.1
 local __icon = LibStub("LibDBIcon-1.0")
-local __LDB = LibStub("LibDataBroker-1.1")
-
-
+local __LDB = LibStub("LibDataBroker-1.1"):NewDataObject("CovenantParty", {
+    type = "launcher",
+    text = "Covenant Party",
+    icon = "Interface\\AddOns\\CovenantParty\\images\\covenantparty",
+    OnClick = function(self, button)
+        if button == "LeftButton" then
+            -- Enable Addon (if not already enabled)
+            if not CovenantParty.addonEnabled then
+                LoadAddOn("CovenantParty")
+            end
+        elseif button == "RightButton" then
+            -- Disable Adddon (if not already disabled)
+            if  CovenantParty.addonEnabled then
+                DisableAddOn("CovenantParty")                    
+            end
+        end
+        CovenantParty.addonEnabled = not CovenantParty.addonEnabled
+        ReloadUI()
+    end,
+    OnTooltipShow = function(_)
+        _:AddLine("Covenant Party", 1, 1, 1)
+        _:AddLine("version: "..CovenantParty.version, 0,2,1,1)
+        _:AddLine(" ")
+        _:AddLine(CovenantParty.L.MinimapTooltip)
+    end
+})
 
 --> Version
     do
@@ -47,7 +100,7 @@ local __LDB = LibStub("LibDataBroker-1.1")
         local expansion, majorPatch, minorPatch = (version or "9.0.0"):match("^(%d+)%.(%d+)%.(%d+)")
 
         -- Check version for support
-        if expansion ~= 9 then
+        if expansion ~= "9" then
             CovenantParty.passVersionCheck = false
         else
             local standarized_version = math.floor((expansion * 10000) + (majorPatch * 100) + (minorPatch))
@@ -60,94 +113,54 @@ local __LDB = LibStub("LibDataBroker-1.1")
         end
     end
 
---> Shared Variable
-    do
-        local defaults = 
-        {
-            character = 
-            {
-                firstUse = true,
-                covenantID = ...,
-                covenantData = 
-                {
-                    textureKit = ...,
-                    celebrationSoundKit = ...,
-                    animaChannelSelectSoundKit = ...,
-                    animaChannelActiveSoundKit = ...,
-                    animaGemsFullSoundKit = ...,
-                    animaNewGemSoundKit = ...,
-                    animaReinforceSelectSoundKit = ...,
-                    name = ...,                    
-                    last_betrayal = ...,
-                    total_betrayals = 0,
-                }
-            },
-            profile = 
-            {
-                minimap = 
-                {
-                    hide = true
-                }
-            },
-            global = 
-            {
-                region = ...
-            }
-        }
-
-        -- Using AceDB-3.0 for SharedVariable management (create and set defaults)
-        CovenantParty.DB = LibSub("AceDB-3.0"):New("CovenantPartyDB", defaults)
-
-        -- Register callback for when user changes profiles
-        CovenantParty.DB.RegisterCallback(CovenantParty, "OnProfileChanged", "RefreshConfiguration")
-        CovenantParty.DB.RegisterCallback(CovenantParty, "OnProfileCopied", "RefreshConfiguration")
-        CovenantParty.DB.RegisterCallback(CovenantParty, "OnProfileReset", "RefreshConfiguration")
-    end
-
-
---> Configuration
-
-    -- Create new Data Broker Object
-    __LDB:NewDataObject("CovenantParty", {
-        type = "launcher",
-        text = "Covenant Party",
-        icon =  "Interface\\Addon\\CovenantParty\\images\\covenantparty",
-        OnClick = function(self, button)
-            if button == "LeftButton" then
-                -- Enable Addon (if not already enabled)
-                if not isEnabled() then
-                    EnableAddon("CovenantParty")
-                end
-            elseif button == "RightButton" then
-                -- Disable Adddon (if not already disabled)
-                if  isEnabled() then
-                    DisableAddon("CovenantParty")                    
-                end
-            end
-            ReloadUI()
-        end,
-        OnTooltipShow = function(_)
-            _:AddLine("Covenant Party "..CovenantParty.version, 1, 1, 1)
-            _:AddLine(" ")
-            _:AddLine(CovenantParty.L.MinimapTooltip)
-        end
-    })
-
     -- Callback for OnProfileChanged, OnProfileCopied
     --  and OnProfileReset.
     function CovenantParty:RefreshConfiguration()
 
         -- Check if player setting is to hide minimap or if addon is enables
-        if not IsEnabled() or self.DB.profile.minimap.hide then
+        if CovenantPartyDB.profile.minimap.hide then
             __icon.Hide("CovenantParty")
+            print("Icon Hidden")
         else
-            if CovenantParty.DB.character[covenantId] ~= 0 then
-                __LDB.icon = 'Interface\\Addon\\CovenantParty\\images\\covenantparty_bw'
+            if CovenantPartyDB.char[covenantId] ~= 0 then
+                __LDB.icon = 'Interface\\AddOns\\CovenantParty\\images\\covenantparty'
+                print(__LDB.icon)
             else
-                __LDB.icon = 'Interface\\Addon\\CovenantParty\\images\\covenantparty'
+                __LDB.icon = 'Interface\\AddOns\\CovenantParty\\images\\covenantparty'            
+                print("Icon")
             end            
         end        
     end
 
+--> Global Frame
+CovenantParty.frame = CreateFrame("Frame")
+
+CovenantParty.frame:SetScript("OnEvent", function(self, event, ...)
+    if event == "ADDON_LOADED" and CovenantParty.addonEnabled == true then
+        local addonName = ...
+        if addonName ~= GlobalAddonName then
+            return
+        end
+        CovenantPartyDB = CovenantPartyDB or {}
+
+        for key, value in pairs(defaults) do
+            if CovenantPartyDB[key] == nil then
+                CovenantPartyDB[key] = value
+            end
+        end
+
+        -- Register the Minimap Icon
+        __icon:Register("CovenantParty", __LDB, CovenantPartyDB.profile.minimap.hide)
+        
+        CovenantParty.addonEnabled = true
+
+        CovenantParty:RefreshConfiguration()
+
+        return true
+    end
+end
+)
+
 -- Set global object
-_G[CovenantParty] = CovenantParty
+_G["CovenantParty"] = CovenantParty
+CovenantParty.frame:RegisterEvent("ADDON_LOADED")
--- locales/CovenantParty-enUS.lua
+++ locales/CovenantParty-enUS.lua
@@ -1,11 +1,11 @@
-local L = LibStub("AceLocale-3.0"):NewLocale ("CovenantParty", "enUS", true) 
+local L = LibStub("AceLocale-3.0"):NewLocale("CovenantParty", "enUS", true) 
 if not L then return end 
 
 --> Minimap Tooltip
-    L.MinimapTooltip = 
-    [[Covenant Party is ready to party!
+    L.MinimapTooltip = [[
+Covenant Party is ready to party!
+Left-click will enable the AddOn.
+Right-click will disable the AddOn.
     
-    Left-click will enable the AddOn.
-    Right-click will disable the AddOn.
+Be kind to each other]]
     
-    Don't forget to be kind to each other.]]
